1b51dc4150b6dd2c42df65b8bc1165ac54276e2d
Mike Sadler <101577043+mtsadler-branch@users.noreply.github.com>
2022-07-09 15:57:36 -0500

    ran black formatter on python code

--- backend/helpers.py
+++ backend/helpers.py
@@ -6,21 +6,21 @@ def get_results(prefix):
     }
     return table_results
 
+
 results = get_results(prefix="a")
 print(results)
 
 
-
 def count_words_in_file(file_name):
     word_counts = {}
-    with open(file_name,'r') as file:
-      for line in file:
-        word_list = line.replace(',','').replace('\'','').replace('.','').lower().split()
-        for word in word_list:
-          # Adding  the word into the wordCounter dictionary.
-          if word not in word_counts:
-            word_counts[word] = 1
-          else:
-            # if the word is already in the dictionary update its count.
-            word_counts[word] = word_counts[word] + 1
+    with open(file_name, "r") as file:
+        for line in file:
+            list_of_words = (
+                line.replace(",", "").replace("'", "").replace(".", "").lower().split()
+            )
+            for word in list_of_words:
+                if word in word_counts:
+                    word_counts[word] = word_counts[word] + 1
+                else:
+                    word_counts[word] = 2
     return word_counts
