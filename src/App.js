8bea3fad5ff2c0dda9a95574a04f98c39e0b46ac
Siddarth Patel <siddarthpatel@Siddarths-MBP.lan1>
2022-07-09 16:01:05 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super();
-
-    // Autosuggest is a controlled component.
-    // This means that you need to provide an input value
-    // and an onChange handler that updates this value (see below).
-    // Suggestions also need to be provided to the Autosuggest,
-    // and they are initially empty because the Autosuggest is closed.
     this.state = {
       value: '',
       suggestions: []
@@ -21,16 +15,13 @@ class App extends React.Component {
       value: newValue
     });
   };
-
-  // Autosuggest will call this function every time you need to update suggestions.
-  // You already implemented this logic above, so just use it.
+  
   onSuggestionsFetchRequested = ({ value }) => {
     this.setState({
       // suggestions: getSuggestions(value)
     });
   };
 
-  // Autosuggest will call this function every time you need to clear suggestions.
   onSuggestionsClearRequested = () => {
     this.setState({
       suggestions: []
