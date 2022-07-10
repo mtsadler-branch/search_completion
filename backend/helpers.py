2379f4b091eb9c50959c3dd83aea8207b52a1ddd
Mike Sadler <101577043+mtsadler-branch@users.noreply.github.com>
2022-07-09 21:14:59 -0500

    ran 'python3 -m black backend

--- backend/helpers.py
+++ backend/helpers.py
@@ -1,5 +1,6 @@
 import argparse
 
+
 def get_results(prefix, word_counts, limit=5):
     """
     Fetch the top 'limit' words from dataset, starting with prefix.
@@ -12,7 +13,10 @@ def get_results(prefix, word_counts, limit=5):
         mapping of top words and their count
     """
     top_results = {}
-    word_counts = {k: v for k, v in sorted(word_counts.items(), key=lambda item: item[1], reverse=True)}
+    word_counts = {
+        k: v
+        for k, v in sorted(word_counts.items(), key=lambda item: item[1], reverse=True)
+    }
     for word, count in word_counts.items():
         if word.startswith(prefix):
             top_results[word] = count
@@ -51,16 +55,32 @@ sample_limit = 10
 
 
 # Argument Parser
-parser = argparse.ArgumentParser(description="Parses details about what game data to load.")
-parser.add_argument("--prefix", dest="prefix", help="The prefix to recommend words for", default=sample_prefix)
-parser.add_argument("--limit", dest="limit", help="How many top results to return", default=sample_limit)
-parser.add_argument("--word_counts", dest="word_counts", help="Mapping of words to counts", default=sample_word_counts)
+parser = argparse.ArgumentParser(
+    description="Parses details about what game data to load."
+)
+parser.add_argument(
+    "--prefix",
+    dest="prefix",
+    help="The prefix to recommend words for",
+    default=sample_prefix,
+)
+parser.add_argument(
+    "--limit", dest="limit", help="How many top results to return", default=sample_limit
+)
+parser.add_argument(
+    "--word_counts",
+    dest="word_counts",
+    help="Mapping of words to counts",
+    default=sample_word_counts,
+)
 
 # Parse Arguments
 args = parser.parse_args()
 
 # Lookup top words in mapping
-results = get_results(prefix=args.prefix, word_counts=args.word_counts, limit=args.limit)
+results = get_results(
+    prefix=args.prefix, word_counts=args.word_counts, limit=args.limit
+)
 
 print(f"Prefix: '{args.prefix}', Limit: {args.limit}.")
 for item in results:
