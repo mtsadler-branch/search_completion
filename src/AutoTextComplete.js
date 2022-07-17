05133a288317517e7ddee3d336094f4da69f077d
Siddarth Patel <siddarthpatel@Siddarths-MBP.lan1>
2022-07-17 17:00:43 -0400

    add dropdown to text field, add styles

--- src/AutoTextComplete.js
+++ src/AutoTextComplete.js
@@ -1,5 +1,6 @@
 import './auto-text-complete.css';
 import React, { useState } from 'react';
+import { DATA } from './Constants.js'
 
 const AutoTextComplete = () => {
   const [inputText, setInputText] = useState('')
@@ -8,11 +9,28 @@ const AutoTextComplete = () => {
     setInputText(event.target.value)
   };
 
+  const renderList = (option, index) => {
+    return (
+      <li className={'dropdown-item'} name={option.label} value={option.label} key={index}>
+          <label className='dropdown-item-label'>{option.label}</label>
+      </li>
+    )
+}
+
   return (
-    <div>
+    <div className='auto-text-dropdown-container'>
       <h2>Auto text complete input</h2>
       <div className='auto-text-field'>
         <input type="text" id="auto-text" name="auto-text" onChange={(e)=> onChange(e)}></input>
+        {inputText && <div className='auto-text-field-dropdown'>
+          <ul className='dropdown-items'>
+            {DATA.map((option, index)=> {
+                    return (
+                      renderList(option, index)
+                    )
+                  })}
+          </ul>
+        </div>}
       </div>
     </div>
   );
