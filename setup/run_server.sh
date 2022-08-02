bae310df52f0067cfb45c2da2fd7ae380a57a5fa
Ubuntu <ubuntu@notincluded.org>
2022-08-02 02:46:55 +0000

    sleep a lil bit

--- setup/run_server.sh
+++ setup/run_server.sh
@@ -1,8 +1,11 @@
 echo "Startiing Backend"
 
 fuser -k 3000/tcp
+sleep 2
 fuser -k 5000/tcp
+sleep 2
 . .venv/bin/activate
+sleep 5
 python3 backend/main.py & disown
 sleep 5
 echo "Starting Frontend"
