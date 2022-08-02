193f7015de41db763eb5d608bd09e44f548ea2aa
Ubuntu <ubuntu@notincluded.org>
2022-08-02 02:42:20 +0000

    updated search_completion setup script

--- setup/run_server.sh
+++ setup/run_server.sh
@@ -2,7 +2,8 @@ echo "Startiing Backend"
 
 fuser -k 3000/tcp
 fuser -k 5000/tcp
-source setup/run_backend.sh
+. .venv/bin/activate
+python3 backend/main.py & disown
 sleep 5
 echo "Starting Frontend"
 npm start & disown
