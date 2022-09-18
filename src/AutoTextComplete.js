e14cfad0d87bc09c593afe029e048b92ff910788
Mike Sadler <101577043+mtsadler-branch@users.noreply.github.com>
2022-09-17 20:30:28 -0500

    localhost -> ip

--- src/AutoTextComplete.js
+++ src/AutoTextComplete.js
@@ -19,7 +19,7 @@ const AutoTextComplete = () => {
             try {
                 setTimeout(() => {
                     console.log('Fetching Suggestions for Prefix...')
-                    fetch(`http://localhost:5001/prefix/${value}/3`, {mode: 'cors'})
+                    fetch(`http://3.137.87.165:5001/prefix/${value}/3`, {mode: 'cors'})
                         .then(res => res.json())
                         .then(data => {
                             let tempWords = []
@@ -42,7 +42,7 @@ const AutoTextComplete = () => {
             try {
                 console.log('Fetching Description for Word...')
                 setLookup(textContent)
-                fetch(`http://localhost:5001/details/${textContent}`, {mode: 'cors'})
+                fetch(`http://3.137.87.165:5001/details/${textContent}`, {mode: 'cors'})
                     .then(res => res.json())
                     .then(data => {
                         // console.log(JSON.stringify(data));
@@ -60,7 +60,7 @@ const AutoTextComplete = () => {
             try {
                 console.log('Fetching Description for Word...')
                 setLookup(value)
-                fetch(`http://localhost:5001/details/${value}`, {mode: 'cors'})
+                fetch(`http://3.137.87.165:5001/details/${value}`, {mode: 'cors'})
                     .then(res => res.json())
                     .then(data => {
                         // console.log(JSON.stringify(data));
